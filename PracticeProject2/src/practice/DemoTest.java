7fd4cc5d0b15b00ed42c13d28c23f2b55acfd125
shu786alok123 <shu786ash123@gmail.com>
2026-04-21 12:44:36 +0530

    Initial Commit for PracticeProject2

---
+++ PracticeProject2/src/practice/DemoTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("for practice purpose");
+
+	}
+
+}
